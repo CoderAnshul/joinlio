59baeed07990d0dd20e797907e2c46669ce357f9
Anshul Sharma <anshul.sharma6163@gmail.com>
2025-01-14 18:44:53 +0530

    some more style manipulation

--- src/Components/LandingPage/RevealText.jsx
+++ src/Components/LandingPage/RevealText.jsx
@@ -63,7 +63,7 @@ const RevealText = () => {
   return (
     <div ref={containerRef} className="flex py-12 items-center min-h-[600px]">
       <section>
-        <p className="reveal-type lg:!leading-[80px] text-3xl leading-[10vw] sm:text-5xl sm:leading-[8vw] md:text-5xl md:leading-[8vw] lg:text-6xl" data-bg-color="rgb(191 191 191)" data-fg-color="black">
+        <p className="reveal-type lg:!leading-[80px] text-3xl leading-[12vw] xs:text-4xl xs:leading-[9vw] sm:text-5xl sm:leading-[8vw] md:text-5xl md:leading-[8vw] lg:text-6xl" data-bg-color="rgb(191 191 191)" data-fg-color="black">
         The game-changing platform where students and alumni unlock global connections, businesses redefine engagement, and universities elevate student development like never before. Discover a world of collaboration, innovation, and boundless opportunities
         </p>
       </section>
