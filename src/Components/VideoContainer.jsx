a13ac4f0cb8720f5490fcef862712b5537d354b2
Anshul Sharma <anshul.sharma6163@gmail.com>
2025-02-11 21:55:55 +0530

    video added

--- src/Components/VideoContainer.jsx
+++ src/Components/VideoContainer.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import video from "../assets/video/dashboard.mp4";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const VideoContainer = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const video = videoRef.current;
+
+    if (!video) return;
+
+    ScrollTrigger.create({
+      trigger: video,
+      start: "top 80%", // Adjust trigger point as needed
+      end: "bottom 20%",
+      onEnter: () => video.play(),
+      onLeave: () => video.pause(),
+      onEnterBack: () => video.play(),
+      onLeaveBack: () => video.pause(),
+    });
+
+    return () => {
+      ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+    };
+  }, []);
+
   return (
-    <div className="relative z-[50] w-full h-[400px] rounded-3xl flex items-center justify-center bg-black">
+    <div className="relative z-[50] w-full h-fit overflow-hidden rounded-3xl flex items-center justify-center bg-black">
       <video
+        ref={videoRef}
         className="w-full h-full object-cover"
-        src="your-video-file.mp4"
-        // controls
-        autoPlay
-        loop
+        src={video}
         muted
       >
         Your browser does not support the video tag.
