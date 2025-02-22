45d92e6f341cfbee53eb697aac66e995dc38e789
Anshul Sharma <anshul.sharma6163@gmail.com>
2025-02-22 15:24:47 +0530

    feat: enhance FeatureGrid styles with gradient backgrounds and hover effects for improved visual appeal

--- src/Components/LandingPage/FeatureGrid.jsx
+++ src/Components/LandingPage/FeatureGrid.jsx
@@ -7,7 +7,7 @@ const FeaturesGrid = () => {
         
         {/* Students */}
         <div className='flex gap-4 flex-col sm:flex-row'>
-        <div className="flex flex-col justify-between min-h-60 bg-gray-300 rounded-2xl p-6 w-full sm:w-1/2 overflow-hidden">
+        <div className="flex flex-col justify-between min-h-60 bg-gradient-to-br from-sky-400 to-sky-100 rounded-2xl p-6 w-full sm:w-1/2 overflow-hidden hover:shadow-lg transition-shadow duration-300">
           <h3 className="text-lg lg:text-2xl font-black text-black">Students</h3>
           <p className="mt-2 text-sm lg:text-base text-gray-600 w-full break-normal">
             Access everything for free! Dive into a world of global connections,
@@ -18,10 +18,10 @@ const FeaturesGrid = () => {
         </div>
 
         {/* Business Owners */}
-        <div className="flex flex-col justify-between min-h-60 bg-gray-300 rounded-2xl p-6 w-full sm:w-1/2 overflow-hidden">
+        <div className="flex flex-col justify-between min-h-60 bg-gradient-to-br from-sky-100 to-sky-400 rounded-2xl p-6 w-full sm:w-1/2 overflow-hidden hover:shadow-lg transition-shadow duration-300">
           <h3 className="text-lg lg:text-2xl font-black text-black">Business Owners</h3>
           <p className="mt-2 text-sm lg:text-base text-gray-600 w-full break-normal">
-            Only pay when you earn! Use JoinLIO’s commission-only model to
+            Only pay when you earn! Use JoinLIO's commission-only model to
             connect with student customers. No upfront costs—pay only when you make sales,
             aligning your expenses directly with your revenue. Enjoy a secure and trustworthy
             platform that protects your data and respects privacy.
@@ -30,7 +30,7 @@ const FeaturesGrid = () => {
         </div>
 
         {/* University Administrators */}
-        <div className="flex flex-col justify-between min-h-60 bg-gray-300 rounded-2xl p-6 w-full  overflow-hidden">
+        <div className="flex flex-col justify-between min-h-60 bg-gradient-to-br from-sky-100 to-sky-400 rounded-2xl p-6 w-full overflow-hidden hover:shadow-lg transition-shadow duration-300">
           <h3 className="text-lg lg:text-2xl font-black text-black">University Administrators</h3>
           <p className="mt-2 text-sm lg:text-base text-gray-600 w-full break-normal">
             Start with a free trial. Experience how JoinLIO can enhance
