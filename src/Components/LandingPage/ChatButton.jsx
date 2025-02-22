545a2e77cb7bef61549061994099912ba61e6a26
Anshul Sharma <anshul.sharma6163@gmail.com>
2025-02-22 13:53:00 +0530

    new images

---
+++ src/Components/LandingPage/ChatButton.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import chatIcon from "../../assets/images/chat.png"
+
+const ChatButton = () => {
+  return (
+    <button 
+      className="fixed bottom-6 right-6 z-[1000] flex items-center justify-center w-14 h-14 rounded-full bg-[#cae3f7] hover:bg-[#2CA2FB] transition-colors duration-300 shadow-lg"
+      aria-label="Open chat"
+    >
+      <img 
+        src={chatIcon} 
+        alt="Chat icon" 
+        className="w-8 h-8 object-contain"
+      />
+    </button>
+  );
+};
+
+export default ChatButton;
