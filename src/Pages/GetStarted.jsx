97c69d10e742573ff57323cd0dc15f8ddde113c0
Anshul <you@example.com>
2025-03-09 14:32:25 +0530

    update email service ID and template in GetStarted and SendMessage components

--- src/Pages/GetStarted.jsx
+++ src/Pages/GetStarted.jsx
@@ -35,10 +35,10 @@ const GetStarted = () => {
 
     try {
       await emailjs.send(
-        'service_cjihnzk',
-        'template_qi88j3i',
+        'service_kewk8yr',
+        'template_ymwsydf',
         templateParams,
-        "qf9TWxuatyoqyCvLk"
+        "o_l0zTlSddslcQg_o"
       );
 
       // Reset form
