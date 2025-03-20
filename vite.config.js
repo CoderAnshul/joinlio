fa4cddda8fbe20f7477a1cca6eb55d884405d202
DhrumitPanchal <dhrumitpanchal789@gmail.com>
2025-03-20 14:03:24 +0530

    change for error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    strictPort: true,
+    hmr: {
+      protocol: "wss",
+      clientPort: 443, // Forces WebSocket to use the correct port
+    },
+  },
+});
